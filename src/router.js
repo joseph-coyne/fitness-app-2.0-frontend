bef5664f3ea3778bac9eddf5fc9c5144697d6d7e
MikeHenze <yashuachrist@gmail.com>
2018-12-28 16:30:26 -0500

    adding users-show page meals index and routes

--- src/router.js
+++ src/router.js
@@ -1,23 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import MealsIndex from './views/MealsIndex.vue';
+import UsersShow from './views/UsersShow.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', name: 'home', component: Home },
+    { path: "/meals", name: "meals-index", component: MealsIndex },
+    { path: "/users/:id", name: "users-show", component: UsersShow }
   ]
-})
+});
