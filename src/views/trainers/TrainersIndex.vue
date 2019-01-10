2e2036664ca45e4f3166ae8e4395e601fceb2e2d
josephpcoyne <josephpcoyne@gmail.com>
2019-01-10 14:58:02 -0500

    fixes vue2-filters error

--- src/views/trainers/TrainersIndex.vue
+++ src/views/trainers/TrainersIndex.vue
@@ -20,7 +20,7 @@
   					<li v-for="trainer in orderBy(filterBy(trainers, searchText), sortAttribute, sortAscending)" v-bind:key="trainer.id">
   						<div class="item-box">
   							<figure>
-  								<router-link v-bind:to="'/trainers/' + trainer.id"><img :src="(trainer.image_url)" width="263" height="395" alt=""></router-link>
+  								<router-link v-bind:to="'/trainers/' + trainer.id"><img :src="(trainer.avatar)" width="263" height="395" alt=""></router-link>
   							</figure>
   							<div class="item-box-desc">
   								<h4>{{trainer.first_name}} {{trainer.last_name}}</h4>
@@ -55,43 +55,45 @@
 </style>
 
 <script>
-var axios = require("axios");
-export default {
-	data: function() {
-		return {
-			trainers: [],
-			tags: [],
-			searchText: "",
-			sortAttribute: "name",
-			sortAscending: 1,
-			errors: []
-		};
-	},
-	created: function() {
-		axios.get("http://localhost:3000/api/trainers").then(response => {
-			console.log(response.data);
-			this.trainers = response.data;
-		});
-		axios.get("http://localhost:3000/api/tags/").then(response => {
-		  this.tags = response.data.map(a => {
-		    return { name: a.name, id: a.id };
-		  });
-		});
-	},
-	methods: {
-  setCurrentTrainer: function(trainer) {
-    this.currentTrainer = Trainer;
-	},
-	setSortAttribute: function(attribute) {
-		if(this.sortAttribute === attribute) {
-			this.sortAscending = this.sortAscending * -1;
-		} else {
-			this.sortAscending = 1;
+	var axios = require("axios");
+	import Vue2Filters from 'vue2-filters'
+	export default {
+		mixins: [Vue2Filters.mixin],
+		data: function() {
+			return {
+				trainers: [],
+				tags: [],
+				searchText: "",
+				sortAttribute: "name",
+				sortAscending: 1,
+				errors: []
+			};
+		},
+		created: function() {
+			axios.get("http://localhost:3000/api/trainers").then(response => {
+				console.log(response.data);
+				this.trainers = response.data;
+			});
+			axios.get("http://localhost:3000/api/tags/").then(response => {
+			  this.tags = response.data.map(a => {
+			    return { name: a.name, id: a.id };
+			  });
+			});
+		},
+		methods: {
+	  setCurrentTrainer: function(trainer) {
+	    this.currentTrainer = Trainer;
+		},
+		setSortAttribute: function(attribute) {
+			if(this.sortAttribute === attribute) {
+				this.sortAscending = this.sortAscending * -1;
+			} else {
+				this.sortAscending = 1;
+			}
+			this.sortAttribute = attribute;
 		}
-		this.sortAttribute = attribute;
-	}
-},
-	computed: {}
-};
+	},
+		computed: {}
+	};
 </script>
 
